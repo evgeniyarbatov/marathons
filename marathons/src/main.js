b2983a626b4a3a9688ee0f90add3286f541edc6b
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-03-05 15:56:44 +0800

    Added bootstrap and cleanup

--- marathons/src/main.js
+++ marathons/src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
+import Vue, { createApp } from '@vue/compat';
+import { BootstrapVue } from 'bootstrap-vue'
 
-import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
+
+Vue.use(BootstrapVue)
 
 createApp(App).mount('#app')
