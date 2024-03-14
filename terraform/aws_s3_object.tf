9b22fef7477c275372de8f211ec1499c205d4e80
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-03-14 14:23:04 +0800

    Copy files to S3 dir

--- terraform/aws_s3_object.tf
+++ terraform/aws_s3_object.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_object" "static_file" {
   for_each     = fileset(local.website_dir, "**")
   bucket       = aws_s3_bucket.static_website.id
-  key          = each.key
+  key          = "marathons/${each.key}"
   source       = "${local.website_dir}/${each.value}"
   content_type = lookup(local.content_types, regex("\\.[^.]+$", each.value), null)
   etag         = filemd5("${local.website_dir}/${each.value}")
